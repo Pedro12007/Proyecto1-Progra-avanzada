1b76d219bfdee814f29dd53b0ba32d3597d06e62
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-09-03 23:23:35 -0600

    Estructura inicial de clase datos

---
+++ datos.py
@@ -0,0 +1,45 @@
+class Datos:
+    def __init(self, usuarios, cursos, inscripciones, evaluaciones):
+        self.usuarios = usuarios
+        self.cursos = cursos
+        self.inscripciones = inscripciones
+        self.evaluaciones = evaluaciones
+
+    def cargar_cursos(self):
+        pass
+
+    def guardar_usuarios(self):
+        pass
+
+    def cargar_cursos(self):
+        pass
+
+    def guardar_cursos(self):
+        pass
+
+    def cargar_inscripciones(self):
+        pass
+
+    def guardar_inscripciones(self):
+        pass
+
+    def cargar_calificaciones(self):
+        pass
+
+    def guardar_calificaciones(self):
+        pass
+
+    def agregar_usuario(self):
+        pass
+
+    def buscar_usuario(self):
+        pass
+
+    def inscribir_estudiante(self):
+        pass
+
+    def registrar_calificacion(self):
+        pass
+
+    def generar_reporte(self):
+        pass
