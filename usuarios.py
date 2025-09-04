ade89ebdf6184a74f7e503313384dc1fe1257a58
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-09-03 23:09:37 -0600

    Estructura inicial de usuarios

---
+++ usuarios.py
@@ -0,0 +1,41 @@
+from abc import ABC, abstractmethod
+
+class Usuario(ABC):
+    def __init__(self, nombre, correo, fecha_nacimiento, rol):
+        self.nombre = nombre
+        self.correo = correo
+        self.fecha_nacimiento = fecha_nacimiento
+        self.rol = rol
+
+    @abstractmethod
+    def mostrar_info(self):
+        pass
+
+class Estudiante(Usuario):
+    def __init__(self, nombre, correo, fecha_nacimiento, rol, carnet, carrera):
+        super().__init__(nombre, correo, fecha_nacimiento, rol)
+        self.__carnet = carnet
+        self.__carrera = carrera
+
+    def mostrar_info(self):
+        pass
+
+    @property
+    def carnet(self):
+        return self.__carnet
+
+    @property
+    def carrera(self):
+        return self.__carrera
+
+class Instructor(Usuario):
+    def __init__(self, nombre, correo, fecha_nacimiento, rol, codigo_empleado):
+        super().__init__(nombre, correo, fecha_nacimiento, rol)
+        self.__codigo_empleado = codigo_empleado
+
+    def mostrar_info(self):
+        pass
+
+    @property
+    def codigo_empleado(self):
+        return self.__codigo_empleado
