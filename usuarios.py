59d627b9d7803f4d315055dbf39c32da372a9138
Javier Huertas <javiandre2007@gmail.com>
2025-09-10 16:18:04 -0600

    menu de estudiante

--- usuarios.py
+++ usuarios.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-
 class Usuario(ABC):
     def __init__(self, nombre, correo, fecha_nacimiento, rol):
         self.nombre = nombre
@@ -21,7 +20,21 @@ class Estudiante(Usuario):
         pass
 
     def acceder_sistema(self):
-        pass
+        while True:
+            print("1. Inscribirse en un curso.\n"
+                  "2. Consultar mis cursos.\n"
+                  "3. ver mis evaluaciones.\n"
+                  "4. Consultar mis calificaciones.")
+            option = int(input("Ingrese una opcion: "))
+            match option:
+                case 1:
+                    print("-"*10 + "INCRIBIRSE EN UN CURSO. " + "-"*10)
+                    nombre = input("Ingrese nombre: ")
+                    correo = input("Ingrese un correo: ")
+                    fecha_nacimiento = int(input("Ingrese año de nacimiento: "))
+                    carnet = input("Ingrese carnet: ")
+                    carrera = input("Ingrese carrera:  ")
+
 
     @property
     def carnet(self):
