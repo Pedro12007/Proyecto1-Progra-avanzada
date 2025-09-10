7b4536e64cd4da67744560f938ae908039a577f7
Javier Huertas <javiandre2007@gmail.com>
2025-09-10 16:52:30 -0600

    class evaluacion

--- cursos.py
+++ cursos.py
@@ -12,3 +12,15 @@ class Cursos:
     @property
     def lista(self):
         return self.__lista
+
+class Evaluacion:
+    def __init__(self, id, estatus, descripcion, punteo):
+        self.__id = id
+        self.estatus = estatus
+        self.descripcion = descripcion
+        self.punteo = punteo
+    def registrar_calificacion(self):
+        pass
+    @property
+    def id(self):
+        return self.__id
