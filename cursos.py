49d2698029e193fa04803590806c5ce62b5e4869
Javier Huertas <javiandre2007@gmail.com>
2025-09-10 16:48:41 -0600

    class cursos

--- cursos.py
+++ cursos.py
@@ -1,16 +1,14 @@
 class Cursos:
-    def __init__(self, id, name, instructor, list):
+    def __init__(self, id, nombre, instructor, lista):
         self.__id = id
-        self.name = name
+        self.nombre = nombre
         self.instructor = instructor
-        self.__list = list
+        self.__lista = lista
     def inscribir_estudiante(self):
         pass
-    def get_id(self):
+    @property
+    def id(self):
         return self.__id
-    def set_id(self, ID):
-        self.__id = ID
-    def get_list(self):
-        return self.__list
-    def set_list(self, list):
-        self.__list = list
+    @property
+    def lista(self):
+        return self.__lista
