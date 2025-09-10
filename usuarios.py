dc8e663527d3c513a06ebeee5317f8c95243ca71
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-09-10 17:45:17 -0600

    modificación roles

--- usuarios.py
+++ usuarios.py
@@ -1,38 +1,37 @@
 from abc import ABC, abstractmethod
 
 class Usuario(ABC):
-    def __init__(self, nombre, correo, fecha_nacimiento, rol):
+    def __init__(self, nombre, correo, fecha_nacimiento):
         self.nombre = nombre
         self.correo = correo
         self.fecha_nacimiento = fecha_nacimiento
-        self.rol = rol
 
     @abstractmethod
     def mostrar_info(self):
         return f"|Nombre: {self.nombre}|Correo: {self.correo}|Fecha de nacimiento: {self.fecha_nacimiento}|Rol: {self.rol}|"
 
 class Estudiante(Usuario):
-    def __init__(self, nombre, correo, fecha_nacimiento, rol, carnet, carrera):
-        super().__init__(nombre, correo, fecha_nacimiento, rol)
+    def __init__(self, nombre, correo, fecha_nacimiento, carnet, carrera):
+        super().__init__(nombre, correo, fecha_nacimiento)
+        self.__rol = 'estudiante'
         self.__carnet = carnet
         self.__carrera = carrera
         self.cursos=[]
-
-
 
     def mostrar_info(self):
         return super().mostrar_info() + f"Carnet: {self.carnet}|Carrera: {self.carrera}"
 
     def acceder_sistema(self):
+        print('ACCESO AL SISTEMA DE ESTUDIANTE\n')
         while True:
             print("1. Inscribirse en un curso.\n"
                   "2. Consultar mis cursos.\n"
                   "3. ver mis evaluaciones.\n"
                   "4. Consultar mis calificaciones.")
-            option = int(input("Ingrese una opcion: "))
+            option = int(input("Ingrese una opción: "))
             match option:
                 case 1:
-                    print("-"*10 + "INCRIBIRSE EN UN CURSO. " + "-"*10)
+                    print("-"*10 + "INSCRIBIRSE EN UN CURSO. " + "-"*10)
                     nombre = input("Ingrese nombre: ")
                     correo = input("Ingrese un correo: ")
                     fecha_nacimiento = int(input("Ingrese año de nacimiento: "))
@@ -48,9 +47,14 @@ class Estudiante(Usuario):
     def carrera(self):
         return self.__carrera
 
+    @property
+    def rol(self):
+        return self.__rol
+
 class Instructor(Usuario):
-    def __init__(self, nombre, correo, fecha_nacimiento, rol, codigo_empleado):
-        super().__init__(nombre, correo, fecha_nacimiento, rol)
+    def __init__(self, nombre, correo, fecha_nacimiento, codigo_empleado):
+        super().__init__(nombre, correo, fecha_nacimiento)
+        self.__rol = 'instructor'
         self.__codigo_empleado = codigo_empleado
         self.cursos_asignados = []
 
@@ -58,6 +62,7 @@ class Instructor(Usuario):
         return super().mostrar_info()+f"|Codigo de empleado: {self.codigo_empleado}"
 
     def acceder_sistema(self):
+        print('ACCESO AL SISTEMA DE ESTUDIANTE\n')
         while True:
             print("1. Consultar mis cursos asignados")
             print("2. Ver estudiantes inscritos")
@@ -75,3 +80,7 @@ class Instructor(Usuario):
     @property
     def codigo_empleado(self):
         return self.__codigo_empleado
+
+    @property
+    def rol(self):
+        return self.__rol
