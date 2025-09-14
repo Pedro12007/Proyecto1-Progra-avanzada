51c30ecf75b686a5cc49626238cc47125f3da43b
Pedro Monroy <monroy.pedroa07@gmail.com>
2025-09-14 01:20:44 -0600

    Menú adaptado a una clase

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-while True:
-    print("-"*10 + "REGISTRO ESTUDIANTES" + "-"*10+ "\n"
-         "1. Registrar usuario \n"
-         "2. Ingresar Estudiante \n"
-         "3. Ingresar Instructor \n"
-         "4. Administrar cursos \n"
-         "5. Salir")
-    option = int(input("Ingrese una opcion: "))
-    match option:
-        case 1:
-            pass
-        case 2:
-            pass
-        case 3:
-            pass
-        case 4:
-            pass
-        case 5:
-            pass
+class MenuPrincipal:
+    def __init__(self):
+        pass
+
+    def ejecutar_menu(self):
+        while True:
+            print("-" * 10 + "REGISTRO ESTUDIANTES" + "-" * 10 + "\n"
+                 "1. Registrar usuario \n"
+                 "2. Ingresar Estudiante \n"
+                 "3. Ingresar Instructor \n"
+                 "4. Administrar cursos \n"
+                 "5. Salir")
+            option = int(input("Ingrese una opcion: "))
+            match option:
+                case 1:
+                    pass
+                case 2:
+                    pass
+                case 3:
+                    pass
+                case 4:
+                    pass
+                case 5:
+                    print('Saliendo del programa...')
+                    break
+
+menu = MenuPrincipal()
+menu.ejecutar_menu()
